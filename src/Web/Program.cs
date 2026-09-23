938e5ce7bf8bfe0c6e3ada25827212a0129e4ef4
Michael Pedersen <95408499+mpeder75@users.noreply.github.com>
2026-09-23 19:30:36 +0200

    Add condition for in-memory database usage

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -22,7 +22,9 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConsole();
 
-if (builder.Environment.IsDevelopment() || builder.Environment.EnvironmentName == "Docker"){
+if (builder.Environment.IsDevelopment()
+    || builder.Environment.EnvironmentName == "Docker"
+    || builder.Configuration.GetValue<bool>("UseOnlyInMemoryDatabase")){
     // Configure SQL Server (local)
     Microsoft.eShopWeb.Infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services);
 }
